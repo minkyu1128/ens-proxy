a6874a7a49720ddc32631707581f7532f8d623bd
minkyu1128 <minkyu1128@gmail.com>
2022-04-26 14:47:03 +0900

    deploy 스크립트 생성

--- src/main/java/cokr/xit/proxy/post/ProxyPostApplication.java
+++ src/main/java/cokr/xit/proxy/post/ProxyPostApplication.java
@@ -2,12 +2,18 @@ package cokr.xit.proxy.post;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.ApplicationPidFileWriter;
 
 @SpringBootApplication
 public class ProxyPostApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ProxyPostApplication.class, args);
+//        SpringApplication.run(ProxyPostApplication.class, args);
+
+        // PID Setting ...
+        SpringApplication application = new SpringApplication(ProxyPostApplication.class);
+        application.addListeners(new ApplicationPidFileWriter());	//PID(Process ID 작성)
+        application.run(args);
     }
 
 
