b3f5801d148e173e63eb1c06e70c1d66a9ccd4fa
minkyu1128 <minkyu1128@gmail.com>
2022-04-27 17:28:20 +0900

    startup.sh 백그라운드 실행 시 nohub.out 로그파일 생성 안되도록 수정

--- gradle/Deploy/bin/startup.sh
+++ gradle/Deploy/bin/startup.sh
@@ -2,6 +2,6 @@
 
 ./shutdown.sh
 
-echo Ens-Proxy Application Start...
-nohup java -jar -Dport=31011 -Droute_url=https://docs-gw.kakaopay.com ../webapps/proxy-post-0.0.1-SNAPSHOT.jar &
+echo Ens-Proxy Application Start background...
+nohup java -jar -Dport=31011 -Droute_url=https://docs-gw.kakaopay.com ../webapps/proxy-post-0.0.1-SNAPSHOT.jar 1> /dev/null 2>&1 &
 
