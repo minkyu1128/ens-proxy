6964fbab8b2ee09aefd7c3875d641a8f565b5ab8
minkyu1128 <minkyu1128@gmail.com>
2022-04-28 09:48:16 +0900

    nice CI  로그출력

--- src/main/java/cokr/xit/proxy/post/api/presentation/NiceCiController.java
+++ src/main/java/cokr/xit/proxy/post/api/presentation/NiceCiController.java
@@ -3,6 +3,7 @@ package cokr.xit.proxy.post.api.presentation;
 import cokr.xit.proxy.post.api.support.Interop;
 import cokr.xit.proxy.post.core.model.ResponseVO;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Map;
 
+@Slf4j
 @RestController
 @RequiredArgsConstructor
 public class NiceCiController {
@@ -24,10 +26,15 @@ public class NiceCiController {
         String jid = mParam.get("jid");
 
         ResponseVO responseVO = null;
-        if(siteCode == null) responseVO = ResponseVO.errRsltBuilder().errCode("ERR410").errMsg("사이트코드는 필수 조건 입니다.").build(); 
-        else if(sitePw == null) responseVO = ResponseVO.errRsltBuilder().errCode("ERR410").errMsg("사이트패스워드는 필수 조건 입니다.").build(); 
+        if(siteCode == null) responseVO = ResponseVO.errRsltBuilder().errCode("ERR410").errMsg("사이트코드는 필수 조건 입니다.").build();
+        else if(sitePw == null) responseVO = ResponseVO.errRsltBuilder().errCode("ERR410").errMsg("사이트패스워드는 필수 조건 입니다.").build();
         else if(jid == null) responseVO = ResponseVO.errRsltBuilder().errCode("ERR410").errMsg("주민번호는 필수 조건 입니다.").build();
         else responseVO = Interop.getCI(siteCode, sitePw, jid);
+
+        log.info("==================================================================================");
+        log.info("==== nice CI response.... ====");
+        log.info(responseVO.toString());
+        log.info("==================================================================================");
 
         return new ResponseEntity<ResponseVO>(responseVO, HttpStatus.OK);
     }
